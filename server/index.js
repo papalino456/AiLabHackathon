634f9014bf5d2005d1568cae01851dba1085b6df
papalino456 <sebastian.barrio.b@gmail.com>
2022-09-08 00:09:33 -0500

    change everything

---
+++ server/index.js
@@ -0,0 +1,25 @@
+import express, { application } from "express";
+import bodyParser from "body-parser";
+import cors from "cors";
+import mongoose from "mongoose";
+import postRoutes from "./routes/posts.js"
+
+const app = express();
+
+app.use("/posts", postRoutes)
+
+app.use(bodyParser.json({limit: "30mb", extended: true}));
+app.use(bodyParser.urlencoded({limit: "30mb", extended: true}));
+app.use(cors())
+
+// DataBaseConection //////////////////////////////////////////
+
+const CONECTION_URL = 'mongodb+srv://papalino456:sebastianpapalino456@cluster0.1efkxdl.mongodb.net/?retryWrites=true&w=majority'
+const PORT = process.env.PORT || 1234;
+
+mongoose.connect(CONECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true})
+.then(() => app.listen(PORT, () => console.log("Server running")))
+.catch((error) => console.log(error.message))
+
+//////////////////////////////////////////////////////////////
+
