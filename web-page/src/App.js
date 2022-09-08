de4cd49aca89c09a83803e36e5c14b1665727794
papalino456 <sebastian.barrio.b@gmail.com>
2022-09-07 19:51:47 -0500

    implement html to react 1

--- web-page/src/App.js
+++ web-page/src/App.js
@@ -4,32 +4,26 @@ import './App.css';
 
 function App() {
   const [data, setData] = React.useState(null);
-
   React.useEffect(() => {
     fetch("/api")
       .then((res) => res.json())
       .then((data) => setData(data.message));
   }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <p>
-          {!data ? "Loading..." : data}
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div class="division">
+      <img class="img-filt" src="https://c4.wallpaperflare.com/wallpaper/667/517/91/video-game-splatoon-2-inkling-splatoon-wallpaper-preview.jpg"></img>
+    <div class="contenidos">
+        <div class="XD"><b>bumpichimp632</b></div>
+        <div class="info">
+            <li><b>&nbsp;&nbsp;237<br><span class="text-span">Follows</span></br></b></li>
+            <li><b>&nbsp;&nbsp;300k<br><span class="text-span">Followers</span></br></b></li>
+            <li><span class="cont-likes"><b>3K</b></span><span class="text-span"><i class="fas fa-heart"></i></span></li>
+        </div>
+        <div class="div-bot">
+            <button class="boton"><i class="fas fa-plus"></i></button>
+        </div>
+      </div>
+      </div>
   );
 }
 
