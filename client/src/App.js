ac36eb1ebfdcd53876f2506a34e4b6a6961e9d46
papalino456 <sebastian.barrio.b@gmail.com>
2022-09-08 13:53:01 -0500

    add posts, forms, etc

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,21 @@
-import React  from "react";
+import React, { useEffect }  from "react";
 import { Container, AppBar, Typography, Grow, Grid } from "@material-ui/core"
 import Posts from "./components/Posts/Posts"
 import Form from "./components/Form/Form"
 import useStyles from "./styles"
+import { useDispatch } from "react-redux";
+import {getPosts} from './actions/posts'
 
 import logo from "./images/4545454d93f74d68a7b2fb418c08df09-transformed.png"
 
 const App = () => {
     const classes = useStyles();
+
+    const dispatch = useDispatch();
+    
+    useEffect(()=> {
+        dispatch(getPosts());
+    }, [dispatch])
 
     return (
         <Container maxWidth="lg">
@@ -17,11 +25,11 @@ const App = () => {
             </AppBar>
             <Grow in>
                 <Container>
-                    <Grid container justify="space-between" alignItems="stretch" spacing={3}>
-                        <Grid item xs={12} sm={7}>
+                    <Grid container justifyContent="space-between" alignItems="stretch" spacing={3}>
+                        <Grid item xs={12} sm={12}>
                             <Posts/>
                         </Grid>
-
+                        <Form/> {/* MOVER ESTO A LA PAGINA DE PERFIL */}
                     </Grid>
                 </Container>
             </Grow>
--- server/index.js
+++ server/index.js
@@ -6,12 +6,11 @@ import postRoutes from "./routes/posts.js"
 
 const app = express();
 
-app.use("/posts", postRoutes)
-
 app.use(bodyParser.json({limit: "30mb", extended: true}));
 app.use(bodyParser.urlencoded({limit: "30mb", extended: true}));
 app.use(cors())
 
+app.use("/posts", postRoutes)
 // DataBaseConection //////////////////////////////////////////
 
 const CONECTION_URL = 'mongodb+srv://papalino456:sebastianpapalino456@cluster0.1efkxdl.mongodb.net/?retryWrites=true&w=majority'
