d13546f80fa23fb03ebe535bfd752de1fc090e24
papalino456 <sebastian.barrio.b@gmail.com>
2022-09-11 10:13:51 -0500

    changes for deployment

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API = axios.create({ baseURL: 'http://localhost:1234' });
+const API = axios.create({ baseURL: 'https://vr-gram.herokuapp.com' });
 
 API.interceptors.request.use((req) => {
     if(localStorage.getItem('profile')) {
