6376df722902fe05e62805f122be1acffe7ee498
papalino456 <sebastian.barrio.b@gmail.com>
2022-09-11 10:35:23 -0500

    make deployable

--- index.js
+++ index.js
@@ -1,14 +1,19 @@
-import express, { application } from "express";
+import express from "express";
 import bodyParser from "body-parser";
 import cors from "cors";
 import mongoose from "mongoose";
 import postRoutes from "./routes/posts.js"
 import userRoutes from "./routes/user.js"
 import dotenv from 'dotenv'
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+import * as path from 'path'
 
 dotenv.config()
 
 const app = express();
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 app.use(bodyParser.json({limit: "30mb", extended: true}));
 app.use(bodyParser.urlencoded({limit: "30mb", extended: true}));
@@ -17,9 +22,11 @@ app.use(cors())
 app.use("/posts", postRoutes)
 app.use("/user", userRoutes)
 
-app.get("/", (req, res) => {
-    res.send("app is running")
-})
+app.use(express.static(path.join(__dirname, 'client/build')));
+
+app.get("/*", (req, res) => {
+ res.sendFile(path.join(__dirname, "client/build", "index.html"));
+});
 // DataBaseConection //////////////////////////////////////////
 
 const PORT = process.env.PORT || 1234;
