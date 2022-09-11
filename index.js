4f7a5491dfbbf8a04fd24b089bdefd69b79a1e7b
papalino456 <sebastian.barrio.b@gmail.com>
2022-09-11 10:41:51 -0500

    Revert "SDADS"
    This reverts commit c4dfffca29fbffad7be5abf7ae35b4f97a1bbfaf.

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ app.use("/user", userRoutes)
 app.use(express.static(path.join(__dirname, 'client/build')));
 
 app.get("/*", (req, res) => {
- res.sendFile(path.join("client/build", "index.html"));
+ res.sendFile(path.join(__dirname, "client/build", "index.html"));
 });
 // DataBaseConection //////////////////////////////////////////
 
